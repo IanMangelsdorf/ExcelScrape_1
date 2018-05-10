bc4e88a889e5945da842662a09f6c1c881172934
ian.mangelsdorf <mnagelsdorfian@gmail.com>
2018-05-10 14:20:03 +0100

    cleaned up some

--- excelScrape.py
+++ excelScrape.py
@@ -1,63 +1,25 @@
 import glob
 import xlrd
-files = glob.glob("C:\Users\User\Desktop\*.xlsx")
-
-class all_data():
-
-    def
-
-class RCA():
-
-    def weight(self, weight):
-        return self.weight()
-
-    def gv(self,a):
-        pass
-
-    def bv(self):
-        pass
-
-    def pv(self):
-        pass
-
-    def phi_bv(self):
-        pass
-
-    def phi_pv(self):
-        pass
-
-    def phi_ob(self):
-        pass
-
-    def ka(self):
-        pass
-
-    def ka_ob(self):
-        pass
-
-    def plug_id(self):
-        pass
-
-    def plug_depth(self):
-        pass
-
-    def well(self):
-        pass
-
-    def dtime(self):
-        pass
-
-    def id(self):
-        self.plug_depth + self.well + self.dtime
+files = glob.glob("C:\\test\\New folder\\*.xlsx")
+datafile = "C:\\test\\sum.xlsx"
+data = xlrd.open_workbook(datafile)
+sht = data.sheet_by_index(0)
 
 
-a = RCA()
 
-a.weight(100.21)
-print (a.weight)
 
 
 for filename in files:
     workbook = xlrd.open_workbook(filename)
+
     for sheet in workbook.sheets():
-        Data.append(sheet.cell_value(10,8))
+        if sheet.name.find("Results")>0:
+            # Collect Job Data
+
+            # Collect RCA Data
+            for row in range(sheet.nrows):
+                for column in range(sheet.ncols):
+                    if s == sheet.cell(row, column).value:
+                        return (row, column)
+
+            data.append(sheet.cell_value(10,8))
---
+++ rca.py
@@ -0,0 +1,46 @@
+class all_data():
+    pass
+
+class RCA():
+
+    def weight(self, weight):
+        return self.weight()
+
+    def gv(self,a):
+        pass
+
+    def bv(self):
+        pass
+
+    def pv(self):
+        pass
+
+    def phi_bv(self):
+        pass
+
+    def phi_pv(self):
+        pass
+
+    def phi_ob(self):
+        pass
+
+    def ka(self):
+        pass
+
+    def ka_ob(self):
+        pass
+
+    def plug_id(self):
+        pass
+
+    def plug_depth(self):
+        pass
+
+    def well(self):
+        pass
+
+    def dtime(self):
+        pass
+
+    def id(self):
+        self.plug_depth + self.well + self.dtime
