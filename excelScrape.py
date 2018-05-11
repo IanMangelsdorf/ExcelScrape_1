9ea7176f4ed9e556f01d0f473e49a135bf0270c8
ian.mangelsdorf <mnagelsdorfian@gmail.com>
2018-05-11 10:48:33 +0100

    Adding RCA Class Formatting

--- excelScrape.py
+++ excelScrape.py
@@ -4,28 +4,53 @@ import rca
 import csv
 
 files = glob.glob("C:\\test\\New folder\\*.xlsx")
+print (files.count())
 datafile = "C:\\test\\sum.xlsx"
 data = xlrd.open_workbook(datafile)
 sht = data.sheet_by_index(0)
 output = "c:\\test\\allrca.csv"
 
 
-def find_job(sh, find):
+def find_ind(sh, find):
     for r in range(sh.nrows):
         for c in range(sh.ncols):
-            if find == sheet.cell(r, c).value:
-                return  sheet.cell(r, c).value
+            if find == sh.cell(r, c).value:
+                return  sh.cell(r, c).value
 
 
-def find_columns(sh, dFields):
+def find_fields(sh, dFields):
     for f in dFields:
         for r in range(sh.nrows):
             for c in range(sh.ncols):
-                if f == sheet.cell(r, c).value:
+                if f == sh.cell(r, c).value:
                     return  dFields, r+1
 
 def rca_data():
-    pass
+    for filename in files:
+        x = +1
+        workbook = xlrd.open_workbook(filename)
+        print ("scraping workbook " + str(x) + " of " + str(files.count))
+
+        for sheet in workbook.sheets():
+            a = rca.RCA()
+            if sheet.name.find("Results") > 0:
+                # Collect Job Data
+                a.well = find_ind(sheet, "Well")
+                a.Company = find_ind(sheet, "Company")
+                a.Doc = find_ind(sheet, "Document")
+                a.rpt_date = find_ind(sheet, "Date")
+
+            dflieds, toprow = find_fields(sheet, dflieds)
+            # Collect RCA Data
+
+            for f in dflieds:
+
+                for row in range(sheet.nrows):
+                    for column in range(sheet.ncols):
+                        if s == sheet.cell(row, column).value:
+                            pass
+
+        write_csv(output, dataline)
 
 
 def write_csv(writeto, writewhat):
@@ -36,27 +61,5 @@ def write_csv(writeto, writewhat):
     csvfile.close()
 
 
-for filename in files:
-    workbook = xlrd.open_workbook(filename)
-
-    for sheet in workbook.sheets():
-        a = rca.RCA()
-        if sheet.name.find("Results")>0:
-            # Collect Job Data
-            a.well = find_job(sheet,"Well")
-            a.Company  = find_job(sheet,"Company")
-            a.Doc = find_job(sheet,"Document")
-            a.rpt_date = find_job(sheet,"Date")
-
-
-        dflieds, toprow = find_columns(sheet,dflieds)
-        # Collect RCA Data
-
-        for f in dflieds:
-
-            for row in range(sheet.nrows):
-                for column in range(sheet.ncols):
-                    if s == sheet.cell(row, column).value:
-                        pass
-
-    write_csv(output, dataline)
+if __name__ == "__main__":
+    rca_data()
